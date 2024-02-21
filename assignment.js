dd2060780597d7dc731caed5981b625af4d0d82a
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2024-02-21 20:20:03 +0500

    java Assignment

---
+++ assignment.js
@@ -0,0 +1,51 @@
+//chapter - 1
+//q1
+alert("wellcom")
+
+//q2
+alert("Error! please enter valid pasword.")
+
+//q3
+alert("some text\nmore text in a new line"); //copy past from https://stackoverflow.com/questions/1841452/new-line-in-javascript-alert-box
+
+
+//q4
+alert("welcom to js land")
+
+
+
+//chapter - 2
+
+//q-1
+var userName= "Ather Ali"
+
+
+//q-2
+var myName= "Ather Ali Siddiqui"
+alert(myName)
+
+//q3
+var message= "Hello Word"
+alert(message)
+
+//q4
+var Name ="jhon"
+var age = "he is 15 year old"
+var developer="cirtified application developer"
+
+alert(Name)
+alert(age)
+alert(developer)
+
+//q6
+var info= "my email address is "
+var email = "example@example.com"
+
+alert(info + email )
+
+//q7
+var book="A smarter way to learn JavaScript"
+alert("i am trying to learn from the book " + book);
+
+
+document.write("Yah! I can write HTML content through Java Script")
