4436a9091fd18a3f991a90ffc5195bf5e5d4a89e
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2024-03-15 22:25:52 +0500

    more work

---
+++ Array/q-11.js
@@ -0,0 +1,19 @@
+
+
+var studentName = ["stdent-1" , "student-2" , "student-3"]
+
+var number = [320 , 230 , 480]
+
+var p1 = (number[0] / 500)*100;
+
+document.write("score of " +studentName[0] + " is " + number[0] +" and percentage is " + p1)
+
+var p2 = (number[1] / 500)*100;
+
+document.write("<br><br>score of " +studentName[1] + " is " + number[1] +
+" and percentage is " + p2)
+
+var p3 = (number[2] / 500)*100;
+
+document.write("<br><br>score of " +studentName[2] + " is " + number[2] +
+" and percentage is " + p3)
